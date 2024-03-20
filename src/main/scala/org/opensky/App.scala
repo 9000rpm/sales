0040d9643dd7372e8ab5fcefa6cde14ad1ccbda6
kamil <kamil.mahabir@gmail.com>
2024-03-20 21:14:58 +1300

    added read file, process file, output file

--- src/main/scala/org/opensky/App.scala
+++ src/main/scala/org/opensky/App.scala
@@ -1,15 +1,33 @@
 package org.opensky
 
+import org.apache.logging.log4j.{LogManager, Logger}
+import org.apache.spark.sql.SparkSession
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
-  }
 
+  lazy val logger: Logger = LogManager.getLogger(this.getClass)
+
+  def main(args: Array[String]): Unit = {
+    val spark = org.apache.spark.sql.SparkSession.builder
+                .master("local")
+                .appName("Spark CSV Sales Reader")
+                .getOrCreate;
+
+    spark.read
+      .option("header",true)
+      .option("inferSchema",true)
+      .csv("C:/Users/kamil/Downloads/sales_data_sample.csv/sales_data_sample.csv")
+      .createOrReplaceTempView("sales")
+
+
+    val salesAvgDf = spark.sql("select YEAR_ID, PRODUCTLINE, round(avg(sales),2) AVERAGE_SALES_AMT from sales where status='Shipped' group by YEAR_ID, PRODUCTLINE order by YEAR_ID, PRODUCTLINE")
+    salesAvgDf.write
+      .format("csv")
+      .option("header", "true")
+      .mode("overwrite")
+      .save("file:///C:/Users/kamil/Downloads/output.csv")
+  }
 }
